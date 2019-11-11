b2159843e350d11f5c6a12747483e9383932e8e6
Timothy Beck <johndaveleet@gmail.com>
2019-11-11 12:07:23 -0700

    created function that does ROT13 encryption

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 
 
 /**
- * function that turns a string into an anagramized version of the string
+ * function that turns a string into an anagram-ized version of the string
  * @param string string to make into an anagram
  * @returns {string} anagramized string
  */
@@ -17,3 +17,26 @@ function createAnagram(string) {
 	//join the array back into a single string then return
 	return strArr.join(' ');
 }
+
+let test = "Abcdefghijklm nOPQRSTUVWXYZ";
+function encrypt(string) {
+	//encrypt using ROT13
+	let explodedStr = string.split('');
+	for (i=0; i < explodedStr.length; i++) {
+		let char = explodedStr[i];
+		if (char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 77 || char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 109) {
+			explodedStr[i] = explodedStr[i].replace(explodedStr[i], String.fromCharCode(char.charCodeAt(0) + 13));
+		}else if(char.charCodeAt(0) >= 78 && char.charCodeAt(0) <=90 || char.charCodeAt(0) >= 110 && char.charCodeAt(0) <= 122) {
+			explodedStr[i] = explodedStr[i].replace(explodedStr[i], String.fromCharCode(char.charCodeAt(0) - 13));
+		}
+	}
+	return explodedStr.join('');
+}
+console.log(encrypt(test));
+
+// plus 13 chars -> 65-78 && 97-110
+// minus 13 chars -> 79-90 && 111-122
+
+//space : 32
+//capital letters: A-Z : 65-90
+//lowercase letters a-z : 97-122
